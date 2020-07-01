e27b16a781a85830daf883175105e6d21b40c0da
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-07-01 00:09:51 -0300

    Adição de axios e função de busca de repositorios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,51 +1,59 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import { FiChevronRight } from "react-icons/fi";
-import logoImg from '../../assets/logo.svg'
+import api from '../../services/api';
 
 import { Title, Form, Repositories } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
+    event.preventDefault();
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório"/>
+      <Form onSubmit={handleAddRepository}>
+        <input placeholder="Digite o nome do repositório" value={newRepo} onChange={(e) => setNewRepo(e.target.value)}/>
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="/">
-          <img
-            src="https://avatars1.githubusercontent.com/u/36999289?s=460&u=d559d9ec7182bc35c1e3116a7538de40dac6e472&v=4"
-            alt="Samuel Leonel"/>
-          <div>
-            <strong>samuel-lf/ecoleta</strong>
-            <p>Projeto Open Source desenvolvido na Next Level Week 1.0 da Rocketseat utilizando TypeScript, Node, React e React Native</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars1.githubusercontent.com/u/36999289?s=460&u=d559d9ec7182bc35c1e3116a7538de40dac6e472&v=4"
-            alt="Samuel Leonel"/>
-          <div>
-            <strong>samuel-lf/ecoleta</strong>
-            <p>Projeto Open Source desenvolvido na Next Level Week 1.0 da Rocketseat utilizando TypeScript, Node, React e React Native</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars1.githubusercontent.com/u/36999289?s=460&u=d559d9ec7182bc35c1e3116a7538de40dac6e472&v=4"
-            alt="Samuel Leonel"/>
-          <div>
-            <strong>samuel-lf/ecoleta</strong>
-            <p>Projeto Open Source desenvolvido na Next Level Week 1.0 da Rocketseat utilizando TypeScript, Node, React e React Native</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {
+          repositories.map(repository => (
+            <a href="/" key={repository.full_name}>
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}/>
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </a>
+          ))
+        }
       </Repositories>
     </>
   )
